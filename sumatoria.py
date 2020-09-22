5dc5365c425a4c607e38e2919eb3d2d3846257d0
Kevin Zumbado Cruz <kzumbado23@gmail.com>
2020-09-21 18:26:10 -0600

    Se agrega version final de la TC3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,14 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    result= 0
+    for i in range(0, n+1):
+        for j in range (0, i):
+            for k in range (j, j+i):
+                result = result +1
+    return result
+    
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
+    nom = 2*pow(n, 3) + 3*pow(n, 2) + n
+    result= nom/6
+    return int(result)
 
